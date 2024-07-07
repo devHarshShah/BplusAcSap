c90de8d3dfac7b56ef15e95a2e36bdd7a5520813
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-07-07 18:38:43 +0530

    chore: build error solved

--- app/timesheet/[slug]/page.tsx
+++ app/timesheet/[slug]/page.tsx
@@ -574,7 +574,7 @@ const Timesheet = ({ params }: { params: { slug: string } }) => {
           Delete Last Entry
         </button>
         <button className="my-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert('Half Day Leave = 4 hours \nFull Day Leave = 8 hours')}>
-          FAQ's
+          FAQ&apos;s
         </button>
         <table className="table-auto w-full text-black border border-black">
           <thead className="border-black border">
