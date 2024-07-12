dfe08d50a8073eb0495e3dd0faed75fd65dd76c6
Harsh Shah <71080727+devHarshShah@users.noreply.github.com>
2024-07-12 23:53:27 +0530

    chore: changed version

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BPlusAC Beta v0.1.0",
+  title: "BPlusAC Beta v0.2.0",
   description: "Fully functional SAP system",
 };
 
